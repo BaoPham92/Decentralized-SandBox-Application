b4a360508f0b6d58cf85eead4bb457feb9273f06
Cuza <baopham92@yahoo.com>
2019-02-07 22:55:43 -0500

    Start of the web3 smart psuedo contract.

---
+++ src/contracts/SimpleContract.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.5.2;
+
+contract SimpleContract {
+    string public value = "defaultValue";
+    
+    function set(string memory _value) public {
+        value = _value;
+    }
+
+    // Transaction counts (standard) object.
+        // Sign and serialize
+        // Broadcast contract
+
+    // Token transaction struct
+}
